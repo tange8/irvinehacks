4ac0191ed7ce03c8cfbafe32e001d5cdbaedd81d
rozguo <117495028+rozguo@users.noreply.github.com>
2023-10-05 18:32:08 -0700

    Frontend Changes (#5)
    * descriptions added to the readme and frontend files
    
    * minor changes to readme and app.jsx
    
    * updated readme
    
    * grammar fix
    
    ---------
    
    Co-authored-by: Rosalind Guo <rosalindguo@dhcp-v062-040.mobile.uci.edu>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
+/*
+This is the starting point of our application. Here, we can begin coding 
+and transforming this page into whatever best suits our needs. 
+For example, we can start by creating a login page, home page, or an about section; 
+there are many ways to get your application up and running. 
+With App.jsx, we can also define global variables and routes to store information as well as page navigation.
+*/
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -29,7 +36,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
