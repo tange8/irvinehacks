a409511a600374a46a2e17ebbb88e7d1a3581c75
Jenny Phan <jphan4115@gmail.com>
2024-01-27 13:52:24 -0800

    added favicon and icon pngs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,8 @@ import "./index.css";
 /*
 This code renders our project so it can be viewed in a browser. 
 */
+
+// document.getElementById("root") renders the index.html file that is the root file
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
